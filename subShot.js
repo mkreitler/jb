48f6587bea2dbad53f20039fec9008e5481931d4
unknown <mkreitler@hotmail.com>
2015-05-04 02:11:52 -0500

    Basic gameplay complete.

--- subShot.js
+++ subShot.js
@@ -10,7 +10,8 @@ game = {
     MAX_CLOUDS: 8,
     MAX_CLOUD_SCALE: 4,
     STATE: {DEAD: 0, OK: 1},
-    SHIP: {X: 50, Y: 12, SCALE_X: 4, SCALE_Y: 2},
+    SHIP: {X: 50, Y: 12, SCALE_X: 4, SCALE_Y: 2, SUB_OFFSET_Y: 4},
+    TORPEDO: {SPEED: 250, DETONATE_RADIUS: 50, FADE_RADIUS: 200, START_DX: -16, START_DY: 14},
     HIDE_SUB_TIME: 2,
     MISSILE_LAUNCH_X: 16,
     MISSILE_SPEED: 500,
@@ -23,6 +24,8 @@ game = {
     FIREBALL_RAD: 200,
     FIREBALL_TIME: 0.25,
     WINNING_SCORE: 3,
+    SUB_REVEAL_TIME: 1,
+    SUB_SURFACE_HEIGHT: 20,
 
     message: null,
     messageRGB: "0, 0, 0",
@@ -30,8 +33,9 @@ game = {
     yTop: 0,
     laneHeight: 0,
     destroyer: {lane: -1, state: 0, target: -1, score: 0},
-    sub: {lane: -1, state: 0, hidingSpots: [], score: 0},
+    sub: {lane: -1, state: 0, hidingSpots: [], targetLanes: [], score: 0},
     missile: {x: 0, y: 0, animFrames: ["missile01up", "missile01up2"], frame: 0, targetY: 0, targetLane: 0, scale_x: 1, scale_y: 2},
+    torpedo: {x: 0, y: 0, dirX: 0, dirY: 0, glyph: "torpedoLeft", targetX: 0, targetY: 0, targetLane: 0, scale_x: 2, scale_y: 1},
 
     // DEBUG:
     fps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -39,20 +43,33 @@ game = {
 
     // Functions ///////////////////////////////////////////////////////////////
     setUp: function() {
-        // Pre-compute position of clouds.
+
+        // Compute constants used to draw the shipping lanes.
+        this.yTop = Math.round(jb.canvas.height * game.SKY_HEIGHT);
+        this.laneHeight = Math.round((jb.canvas.height - this.yTop) / game.NUM_LANES);
+
+        jb.resize(jb.canvas.width, this.yTop + this.NUM_LANES * this.laneHeight);
+    },
+
+    startGame: function() {
         var numClouds = 0,
             iCloud = 0,
             scale = 1,
             cloudGlyph = null,
             newCloud = null;
 
-        // Compute constants used to draw the shipping lanes.
-        this.yTop = Math.round(jb.canvas.height * game.SKY_HEIGHT);
-        this.laneHeight = Math.round((jb.canvas.height - this.yTop) / game.NUM_LANES);
-
         jb.setBackColor(game.BACK_COLOR);
-        jb.resize(jb.canvas.width, this.yTop + this.NUM_LANES * this.laneHeight);
         jb.clear();
+
+        this.message = null;
+        this.banner = null;
+        this.destroyer.score = 0;
+        this.destroyer.state = this.STATE.OK;
+
+        this.sub.score = 0;
+        this.sub.state = this.STATE.OK;
+        this.sub.hidingSpots.length = 0;
+        this.sub.targetLanes.length = 0;
 
         // Randomly position some clouds (but don't draw them, yet!).
         for (iCloud = 0; iCloud < this.MAX_CLOUDS; ++iCloud) {
@@ -83,17 +100,7 @@ game = {
         jb.fonts.printAt("military", 11, 40, "from which it has already fired.`", "gray", 0.5, 0.0, 1);
         jb.fonts.printAt("military", 13, 40, "<TAP> to start!", "gray", 0.5, 0.0, 2);
 
-        this.message = "<TAP> a blue shipping lane to position your destroyer.";
         jb.listenForTap();
-    },
-
-    setupNewGame: function() {
-        this.destroyer.score = 0;
-        this.destroyer.state = this.STATE.OK;
-
-        this.sub.score = 0;
-        this.sub.state = this.STATE.OK;
-        this.sub.hidingSpots.length = 0;
     },
 
     waitForStartTap_loop: function() {
@@ -103,6 +110,8 @@ game = {
     },
 
     setupPlaceDestroyer: function() {
+        this.messageRGB = "0, 0, 0";
+        this.message = "<TAP> a blue lane to place your ship.";
         jb.listenForTap();
     },
 
@@ -114,17 +123,20 @@ game = {
         if (lane >= 0) {
             this.destroyer.lane = lane;
             this.destroyer.state = this.STATE.OK;
-            jb.startTimer("hideSub");
         }
 
         return lane < 0;
+    },
+
+    setupHideSub: function() {
+        jb.startTimer("hideSub");
     },
 
     hideSub_loop: function() {
         var timePassed = jb.timer("hideSub"),
             bContinueLoop = true;
 
-        this.message = "Sub is hiding";
+        this.message = "New sub arrives!";
 
         if (timePassed < this.HIDE_SUB_TIME * 0.25) {
             this.messageRGB = "0, 0, 0";
@@ -140,9 +152,6 @@ game = {
         }
 
         if (jb.timer("hideSub") > this.HIDE_SUB_TIME) {
-            this.moveSubToNewLane();
-            this.messageRGB = "0, 0, 0";
-            this.message = "<Tap> a lane to fire a missile.";
             bContinueLoop = false;
         }
 
@@ -156,6 +165,9 @@ game = {
         // Proceed to the next phase, where we wait for the player
         // to fire a missile.
         this.messageRGB = "0, 0, 0";
+        this.message = "<Tap> a lane to fire a missile.";
+        this.moveSubToNewLane();
+        this.sub.state = this.STATE.OK;
         jb.listenForTap();
     },
 
@@ -274,12 +286,107 @@ game = {
                 jb.goto("hitSub");
             }
             else {
-                this.message = "Missed. <Tap> to fire again.";
-                jb.goto("setupPlayerShot");
+                this.message = "Missed. Sub attacks!";
             }
         }
 
         return jb.timer("ripples") < this.RIPPLE_TIME;
+    },
+
+    setupSubSurface: function() {
+        jb.startTimer("subSurface");
+    },
+
+    subSurface_loop: function() {
+        var cloakParam = 0;
+
+        cloakParam = Math.min(1.0, jb.timer("subSurface") / this.SUB_REVEAL_TIME);
+
+        this.drawBoard();
+        this.drawDestroyer();
+        this.drawTransitioningSub(cloakParam);
+
+        return cloakParam < 1.0;
+    },
+
+    setupSubFires: function() {
+        var len = 0;
+
+        this.getNewSubTarget();
+
+        this.torpedo.x = jb.canvas.width - this.SHIP.X - 16 * this.SHIP.SCALE_X + this.TORPEDO.START_DX;
+        this.torpedo.y = this.laneToSubY(this.sub.lane) - this.SUB_SURFACE_HEIGHT + this.TORPEDO.START_DY;
+        this.torpedo.targetX = this.SHIP.X;
+        this.torpedo.targetY = this.yFromLane(this.torpedo.targetLane) + Math.round(this.SHIP.Y + 16 * this.SHIP.SCALE_Y * 0.5);
+        this.torpedo.dirX = this.torpedo.targetX - this.torpedo.x;
+        this.torpedo.dirY = this.torpedo.targetY - this.torpedo.y;
+        len = Math.sqrt(this.torpedo.dirX * this.torpedo.dirX + this.torpedo.dirY * this.torpedo.dirY);
+        this.torpedo.dirX /= len;
+        this.torpedo.dirY /= len;
+
+        jb.startTimer("subSurface");
+    },
+
+    subFires_Loop: function() {
+        var cloakParam = 0,
+            dx = 0,
+            dy = 0,
+            distToTarget = 0;
+
+        cloakParam = 1.0 - Math.min(1.0, jb.timer("subSurface") / this.SUB_REVEAL_TIME);
+
+        this.torpedo.x += this.torpedo.dirX * jb.time.deltaTime * this.TORPEDO.SPEED;
+        this.torpedo.y += this.torpedo.dirY * jb.time.deltaTime * this.TORPEDO.SPEED;
+
+        dx = this.torpedo.targetX - this.torpedo.x;
+        dy = this.torpedo.targetY - this.torpedo.y;
+
+        distToTarget = Math.sqrt(dx * dx + dy * dy);
+
+        this.drawBoard();
+
+        if (distToTarget < this.TORPEDO.FADE_RADIUS) {
+            jb.ctxt.globalAlpha = distToTarget / this.TORPEDO.FADE_RADIUS;
+        }
+        this.drawTorpedo();
+        jb.ctxt.globalAlpha = 1.0;
+        this.drawDestroyer();
+
+        this.drawTransitioningSub(cloakParam);
+
+        return dx * this.torpedo.dirX + dy * this.torpedo.dirY > 0;
+    },
+
+    checkTorpedoHitsPlayer: function() {
+        if (this.torpedo.targetLane !== this.destroyer.lane) {
+            jb.goto("setupPlayerShot");
+        }
+    },
+
+    setupHitDestroyer_loop: function() {
+        jb.startTimer("explosion");
+        jb.startTimer("subSurface");
+    },
+
+    hitDestroyer_loop: function() {
+        // Fireball.
+        var x = Math.round(this.torpedo.x + 16 * this.SHIP.SCALE_X * 0.5),
+            y = Math.round(this.torpedo.y + 8 * this.SHIP.SCALE_Y * 0.5);
+
+        this.drawBoard();
+        this.drawFireball(jb.timer("explosion"), this.torpedo.targetLane, x, y);
+
+        jb.ctxt.globalAlpha = 1.0 - Math.min(1.0, jb.timer("explosion") / this.FIREBALL_TIME);
+        this.drawDestroyer();
+        jb.ctxt.globalAlpha = 1.0;
+
+        return jb.timer("explosion") < this.FIREBALL_TIME;
+    },
+
+    resolveHitDestroyer: function() {
+        this.sub.score += 1;
+        this.destroyer.state = this.STATE.DEAD;
+        jb.goto("setupEndRound");
     },
 
     hitSub: function() {
@@ -290,36 +397,11 @@ game = {
 
     hitSub_loop: function() {
         // Fireball.
-        var rad = 0,
-            x = Math.round(this.missile.x + 8 * this.missile.scale_x * 0.5),
-            y = Math.round(this.missile.y - 8 * this.missile.scale_y * 0.5),
-            yStart = Math.round(y + this.FIREBALL_RAD * 0.0167),
-            alpha = Math.max(0, 1.0 - jb.timer("explosion") / this.FIREBALL_TIME);
+        var x = Math.round(this.missile.x + 8 * this.missile.scale_x * 0.5),
+            y = Math.round(this.missile.y - 8 * this.missile.scale_y * 0.5);
 
         this.drawBoard();
-
-        rad = Math.round(jb.timer("explosion") * this.FIREBALL_RAD);
-        y -= Math.round(jb.timer("explosion") * this.laneHeight * 0.1);
-
-        jb.ctxt.save();
-
-        jb.ctxt.beginPath();
-        jb.ctxt.fillStyle = "rgba(255, 128, 0, " + alpha + ")";
-        jb.ctxt.arc(x, y, rad, 0, 2 * Math.PI, false);
-        jb.ctxt.fill();
-
-        jb.ctxt.beginPath();
-        jb.ctxt.fillStyle = this.getLaneColor(this.missile.targetLane);
-        jb.ctxt.fillRect(x - rad, yStart, 2 * rad, this.yForLane(this.missile.targetLane + 1) - yStart);
-
-        this.drawLane(this.missile.targetLane + 1);
-
-        jb.ctxt.beginPath();
-        jb.ctxt.fillStyle = "rgba(255, 255, 255, " + Math.sqrt(alpha) + ")";
-        jb.ctxt.fillRect(x - Math.round(rad * 1.25), yStart, Math.round(2.5 * rad), 1);
-
-        jb.ctxt.restore();
-
+        this.drawFireball(jb.timer("explosion"), this.missile.targetLane, x, y);
         this.drawDestroyer();
 
         return jb.timer("explosion") < this.FIREBALL_TIME;
@@ -330,37 +412,33 @@ game = {
 
         if (this.sub.state === this.STATE.DEAD) {
             this.messageRGB = "0, 128, 0";
-            this.message = "You hit the sub! Score: " + this.destroyer.score + " to " + this.sub.score;
+            this.banner = "Hit!";
+            this.message = "Score: " + this.destroyer.score + " to " + this.sub.score + ". <TAP> to continue.";
         }
         else if (this.destroyer.state === this.STATE.DEAD) {
             this.messageRGB = "255, 0, 0";
-            this.message = "You got hit! Score: " + this.destroyer.score + " to " + this.sub.score;
+            this.message = "Score: " + this.destroyer.score + " to " + this.sub.score + ". <TAP> to continue.";
         }
     },
 
     endRound_loop: function() {
         this.drawBoard();
-
-        if (this.destroyer.score >= this.WINNING_SCORE) {
-            this.drawDestroyer();
-        }
-
-        if (this.sub.score >= this.WINNING_SCORE) {
-            this.drawSub();
-        }
+        this.drawDestroyer();
 
         return !jb.tap.done;
     },
 
     endRound: function() {
+        this.banner = null;
+
         if (this.destroyer.score >= this.WINNING_SCORE ||
             this.sub.score >= this.WINNING_SCORE) {
             jb.goto("gameOver");
         }
         else if (this.sub.state === this.STATE.DEAD) {
-            jb.goto("hideSub_loop");
+            jb.goto("setupHideSub");
         }
-        else if (this.destroyer === this.STATE.DEAD) {
+        else if (this.destroyer.state === this.STATE.DEAD) {
             jb.goto("setupPlaceDestroyer");
         }
     },
@@ -390,14 +468,13 @@ game = {
 
         if (this.sub.score >= this.WINNING_SCORE) {
             this.banner = "YOU LOSE";
-            this.drawSub();
         }
 
         return !jb.tap.done;
     },
 
     playAgain: function() {
-        jb.goto("setupNewGame");
+        jb.goto("startGame");
     },
 
     // Helper Functions ///////////////////////////////////////////////////////
@@ -445,8 +522,42 @@ game = {
         return lane * this.laneHeight + this.yTop;
     },
 
+    laneToSubY: function(lane) {
+        return this.yFromLane(lane) + this.laneHeight - 1 - 16 * this.SHIP.SCALE_Y + this.SHIP.SUB_OFFSET_Y;
+    },
+
     getLaneColor: function(lane) {
         return "rgba(0, 0, " + 25 * (lane + 1) + ", 1)";
+    },
+
+    drawFireball: function(t, lane, x, y) {
+        var rad = 0,
+            yStart = Math.round(y + this.FIREBALL_RAD * 0.0167),
+            alpha = Math.max(0, 1.0 - t / this.FIREBALL_TIME);
+
+        if (t < this.FIREBALL_TIME) {
+            rad = Math.round(t * this.FIREBALL_RAD);
+            y -= Math.round(t * this.laneHeight * 0.1);
+
+            jb.ctxt.save();
+
+            jb.ctxt.beginPath();
+            jb.ctxt.fillStyle = "rgba(255, 128, 0, " + alpha + ")";
+            jb.ctxt.arc(x, y, rad, 0, 2 * Math.PI, false);
+            jb.ctxt.fill();
+
+            jb.ctxt.beginPath();
+            jb.ctxt.fillStyle = this.getLaneColor(lane);
+            jb.ctxt.fillRect(x - rad, yStart, 2 * rad, this.yForLane(lane + 1) - yStart);
+
+            this.drawLane(lane + 1);
+
+            jb.ctxt.beginPath();
+            jb.ctxt.fillStyle = "rgba(255, 255, 255, " + Math.sqrt(alpha) + ")";
+            jb.ctxt.fillRect(x - Math.round(rad * 1.25), yStart, Math.round(2.5 * rad), 1);
+
+            jb.ctxt.restore();
+        }
     },
 
     drawBoard: function() {
@@ -475,8 +586,8 @@ game = {
         }
 
         if (this.message) {
-            jb.colorRows("rgba(" + this.messageRGB + ", 0.85)", 3);
-            jb.fonts.printAt("military", 3, 40, this.message, "white", 0.5, 0.5, 1);
+            jb.colorRows("rgba(" + this.messageRGB + ", 0.85)", 3, 4);
+            jb.fonts.printAt("military", 3, 40, this.message, "white", 0.5, 0.5, 2);
         }
 
         if (this.banner) {
@@ -502,15 +613,24 @@ game = {
         }
     },
 
-    drawSub: function() {
+    drawSub: function(subY) {
         if (this.sub.state === this.STATE.OK) {
             jb.glyphs.draw("16x16",
                            "submarineLeft",
                            jb.canvas.width - this.SHIP.X - 16 * this.SHIP.SCALE_X,
-                           this.yFromLane(this.sub.lane) + this.SHIP.Y,
+                           subY || this.laneToSubY(this.sub.lane),
                            this.SHIP.SCALE_X,
                            this.SHIP.SCALE_Y);
         }
+    },
+
+    drawTorpedo: function() {
+        jb.glyphs.draw("8x8",
+                       this.torpedo.glyph,
+                       this.torpedo.x,
+                       this.torpedo.y,
+                       this.torpedo.scale_x,
+                       this.torpedo.scale_y);
     },
 
     drawMissile: function() {
@@ -520,6 +640,30 @@ game = {
                        this.missile.y,
                        this.missile.scale_x,
                        this.missile.scale_y);
+    },
+
+    drawTransitioningSub: function(cloakParam) {
+        var yCloak = 0,
+            laneBottom = this.yFromLane(this.sub.lane) + this.laneHeight - 1,
+            subX = 0,
+            subY = 0;
+
+        subX = jb.canvas.width - this.SHIP.X - 16 * this.SHIP.SCALE_X;
+        subY = this.laneToSubY(this.sub.lane) - this.SUB_SURFACE_HEIGHT * cloakParam;
+
+        jb.ctxt.globalAlpha = cloakParam;
+        this.drawSub(subY);
+        jb.ctxt.globalAlpha = 1.0;
+
+        yCloak = this.laneToSubY(this.sub.lane) + Math.round(0.25 * 16 * this.SHIP.SCALE_Y);
+
+        jb.ctxt.save();
+        jb.ctxt.beginPath();
+        jb.ctxt.fillStyle = this.getLaneColor(this.sub.lane);
+        jb.ctxt.fillRect(subX, yCloak, 16 * this.SHIP.SCALE_X, laneBottom - yCloak);
+        jb.ctxt.fill();
+
+        jb.ctxt.restore();
     },
 
     moveSubToNewLane: function() {
@@ -550,6 +694,36 @@ game = {
 
         this.sub.lane = lane;
         this.sub.hidingSpots.push(this.sub.lane);
+    },
+
+    getNewSubTarget: function() {
+        var bHidden = false,
+            lane = 0,
+            i = 0;
+
+        lane = Math.floor(Math.random() * this.NUM_LANES);
+        while (!bHidden) {
+            bHidden = true;
+
+            // Look through the target lanes already used...
+            for (i=0; i<this.sub.targetLanes.length; ++i) {
+                // ...if this is a used lane...
+                if (this.sub.targetLanes[i] === lane) {
+                    // ...move to the next lane...
+                    lane = lane + 1;
+
+                    // ...wrap around, if necessary...
+                    lane = lane % this.NUM_LANES;
+
+                    // ...and try again with this new lane.
+                    bHidden = false;
+                    break;
+                }
+            }
+        }
+
+        this.torpedo.targetLane = lane;
+        this.sub.targetLanes.push(lane);
     }
 };
 
